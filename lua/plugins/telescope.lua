3a86814dab4169a07cebdc75258dbb15d82ea011
Hyunseo Ha <hyunseo.ha@th-deg.de>
2024-03-12 11:54:26 +0100

    lsp added

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,5 @@
 return	{
-		'nvim-telescope/telescope.nvim', tag = '0.1.5', 
+	{		'nvim-telescope/telescope.nvim', tag = '0.1.5', 
 		dependencies = { 'nvim-lua/plenary.nvim' },
 	config = function()
 		local builtin = require('telescope.builtin')
@@ -9,4 +9,19 @@ return	{
 			builtin.grep_string({ search = vim.fn.input("Grep > ")});
 		end)
 		end
-	}
+	},
+	{
+		'nvim-telescope/telescope-ui-select.nvim',
+		config = function ()
+		require("telescope").setup ({
+			extensions = {
+				["ui-select"] = {
+					require("telescope.themes").get_dropdown {
+					}
+				}
+			}
+		})
+		require("telescope").load_extension("ui-select")
+		end
+	},
+}
