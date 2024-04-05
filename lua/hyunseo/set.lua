7119d7f0b2bc306a41e195835694a89fbea78deb
Hyunseo Ha <hyunseo.ha@th-deg.de>
2024-04-05 10:13:52 +0200

    nvim-tmux-navigator applied

--- lua/hyunseo/set.lua
+++ lua/hyunseo/set.lua
@@ -7,4 +7,8 @@ vim.opt.incsearch = true
 vim.opt.scrolloff = 10
 vim.opt.signcolumn = "yes"
 
-vim.opt.colorcolumn = "80"
+-- navigate vim panes better
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
